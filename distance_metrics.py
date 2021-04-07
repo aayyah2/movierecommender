b9c8e6b2a3e5bf58b086c8089efe2ea86d827048
Ayyah, Aparna Lakshmi <aayyah2@illinois.edu>
2021-04-07 11:12:48 -0500

    Create distance_metrics.py

---
+++ distance_metrics.py
@@ -0,0 +1,7 @@
+def euclidean_distance(vec1, vec2):
+  #return euclidean distance between vec1 and vec2
+  return 0
+
+def cosine_similarity(vec1, vec2):
+  #return cosine_similarity value between vec1 and vec2
+  return 0
