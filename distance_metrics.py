72c2b945e15855c74e7939a8ef20038784fcaf62
Mahi Kolla <mkolla2@DESKTOP-HVB2JTH.localdomain>
2021-04-09 12:55:45 -0700

    Implemented euclidean_distance and cosine_similarity functions

--- distance_metrics.py
+++ distance_metrics.py
@@ -1,7 +1,15 @@
+import numpy as np
+
 def euclidean_distance(vec1, vec2):
   #return euclidean distance between vec1 and vec2
-  return 0
+  distance = np.linalg.norm(vec1 - vec2)
+  return np.sqrt(distance)
 
 def cosine_similarity(vec1, vec2):
   #return cosine_similarity value between vec1 and vec2
-  return 0
+  dot = np.dot(vec1, vec2)
+  mag1 = np.linalg.norm(vec1)
+  mag2 = np.linalg.norm(vec2)
+  cosTheta = dot/(mag1 * mag2)
+  theta = np.arccos(cosTheta)
+  return theta
